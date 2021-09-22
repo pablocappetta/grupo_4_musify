89630ee76de8cdac036d612141ca9617dd3cf7af
Pablo <75391203+pablocappetta@users.noreply.github.com>
2021-09-21 21:44:26 -0300

    added core files

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.listen(45345, () => {
+  console.log("Server initiated in port 42133");
+  console.log("Hostname: http://localhost:42133");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/register.html"));
+});
+
+app.post("/register", (req, res) => {
+  res.redirect("/");
+});
+
+app.use(express.static("public"));
