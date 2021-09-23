012a217df0e4a1d2ebff0f7caa45e3b12f1d56d8
Pablo <75391203+pablocappetta@users.noreply.github.com>
2021-09-23 16:37:47 -0300

    modified core HTML files

--- app.js
+++ app.js
@@ -11,12 +11,16 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/home.html"));
 });
 
-app.get("/register", (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/register.html"));
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/login.html"));
 });
 
-app.post("/register", (req, res) => {
-  res.redirect("/");
+app.get("/cart", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/cart.html"));
+});
+
+app.get("/products", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/products.html"));
 });
 
 app.use(express.static("public"));
