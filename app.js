563ec4eb20be446891790354e7bc8399c45d53bf
Pablo <75391203+pablocappetta@users.noreply.github.com>
2021-09-25 20:17:17 -0300

    added core files for header

--- app.js
+++ app.js
@@ -15,12 +15,24 @@ app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/login.html"));
 });
 
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/register.html"));
+});
+
 app.get("/cart", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/cart.html"));
 });
 
 app.get("/products", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/products.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/contact.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/about.html"));
 });
 
 app.use(express.static("public"));
