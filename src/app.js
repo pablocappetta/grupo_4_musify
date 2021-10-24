cb9ae50f455d9f95f503386bc30c15f102cffc24
Felix-Hz <feluhv@outlook.es>
2021-10-24 17:30:48 -0300

    Introduced EJS logic, and updated the views with HEAD, HEADER, FOOTER templates

--- src/app.js
+++ src/app.js
@@ -11,31 +11,31 @@ app.set('view engine', 'ejs'); // Define que el motor que utilizamos es EJS
 app.set('views', path.join(__dirname, '/views')); // Define la ubicación de la carpeta de las Vistas
 
 app.get("/", (req, res) => {
-  res.render(path.join(__dirname, "/views/users/index.ejs")); // Con EJS se remplaza el SendFile por el .render
+  res.render(path.join(__dirname, "/views/users/index")); // Con EJS se remplaza el SendFile por el .render
 });
 
 app.get("/login", (req, res) => {
-  res.render(path.join(__dirname, "/views/users/login.ejs"));
+  res.render(path.join(__dirname, "/views/users/login"));
 });
 
 app.get("/register", (req, res) => {
-  res.render(path.join(__dirname, "/views/users/register.ejs"));
+  res.render(path.join(__dirname, "/views/users/register"));
 });
 
 app.get("/cart", (req, res) => {
-  res.render(path.join(__dirname, "/views/products/cart.ejs"));
+  res.render(path.join(__dirname, "/views/products/cart"));
 });
 
 app.get("/products", (req, res) => {
-  res.render(path.join(__dirname, "/views/products/products.ejs"));
+  res.render(path.join(__dirname, "/views/products/products"));
 });
 
 app.get("/contact", (req, res) => {
-  res.render(path.join(__dirname, "/views/users/contact.ejs"));
+  res.render(path.join(__dirname, "/views/users/contact"));
 });
 
 app.get("/about", (req, res) => {
-  res.render(path.join(__dirname, "/views/users/about.ejs"));
+  res.render(path.join(__dirname, "/views/users/about"));
 });
 
 app.use(express.static("public"));
