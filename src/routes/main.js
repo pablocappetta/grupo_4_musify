c2f4a6b2fa170497ecfab1ed869b7a99b78e325e
Diego <diegocej@gmail.com>
2021-10-24 20:56:56 -0300

    Add controller/router-> products v1.1

--- src/routes/main.js
+++ src/routes/main.js
@@ -7,8 +7,6 @@ const mainController = require('../controllers/mainController');
 router.get('/', mainController.index); 
 router.get('/login', mainController.login); 
 router.get('/register', mainController.register); 
-router.get('/cart', mainController.cart); 
-router.get('/products', mainController.products); 
 router.get('/contact', mainController.contact); 
 router.get('/about', mainController.about);
 
