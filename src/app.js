26e6536401bb00ef70b45a0d52ffc9398ce05caf
Diego <diegocej@gmail.com>
2021-10-24 20:50:12 -0300

    finish controller and routers v1.0

--- src/app.js
+++ src/app.js
@@ -2,41 +2,22 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+app.use(express.static("public"));
+
+//process.env.PORT -> Heroku PORT
 app.listen(process.env.PORT || 42133, () => {
   console.log("Server initiated in port 42133");
   console.log("Hostname: http://localhost:42133");
 });
 
-app.set('view engine', 'ejs'); // Define que el motor que utilizamos es EJS 
-app.set('views', path.join(__dirname, '/views')); // Define la ubicación de la carpeta de las Vistas
+// Define que el motor que utilizamos es EJS 
+app.set('view engine', 'ejs');                   
+// Define la ubicación de la carpeta de las Vistas       
+app.set('views', path.join(__dirname, '/views'));       
 
-app.get("/", (req, res) => {
-  res.render(path.join(__dirname, "/views/users/index")); // Con EJS se remplaza el SendFile por el .render
-});
+// Rutas 
+const mainRouter = require('./routes/main');
+// const productsRouter = require('./routes/products');
 
-app.get("/login", (req, res) => {
-  res.render(path.join(__dirname, "/views/users/login"));
-});
-
-app.get("/register", (req, res) => {
-  res.render(path.join(__dirname, "/views/users/register"));
-});
-
-app.get("/cart", (req, res) => {
-  res.render(path.join(__dirname, "/views/products/cart"));
-});
-
-app.get("/products", (req, res) => {
-  res.render(path.join(__dirname, "/views/products/products"));
-});
-
-app.get("/contact", (req, res) => {
-  res.render(path.join(__dirname, "/views/users/contact"));
-});
-
-app.get("/about", (req, res) => {
-  res.render(path.join(__dirname, "/views/users/about"));
-});
-
-app.use(express.static("public"));
-
+app.use('/',mainRouter);
+// app.use('/products'),
---
+++ src/controllers/mainController.js
@@ -0,0 +1,42 @@
+// utilizo libreria path para obtener la ruta 
+const path = require('path');
+
+const mainController = {
+
+    index: (req, res) => {
+        let archivo = path.join(__dirname, "../views/users/index");
+        res.render(archivo);
+      },
+      
+    login: (req, res) => {
+        let archivo = path.join(__dirname, "../views/users/login");
+        res.render(archivo);
+      },
+      
+    register: (req, res) => {
+        let archivo = path.join(__dirname, "../views/users/register");
+        res.render(archivo);
+      },
+      
+    cart:(req, res) => {
+        let archivo = path.join(__dirname, "../views/products/cart");
+        res.render(archivo);
+      },
+      
+    products: (req, res) => {
+        let archivo = path.join(__dirname, "../views/products/products");
+        res.render(archivo);
+      },
+      
+    contact: (req, res) => {
+        let archivo = path.join(__dirname, "../views/users/contact");
+        res.render(archivo);
+      },
+      
+    about:(req, res) => {
+        let archivo = path.join(__dirname, "../views/users/about");
+        res.render(archivo);
+      }
+};
+
+module.exports = mainController;
