357d0bb7ecb2d4d6c18a7c458a392b7190394a19
Felix-Hz <feluhv@outlook.es>
2021-11-28 15:27:55 -0300

    installed bcrypt. applied register-psw hashing.

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,7 @@
 // utilizo libreria path para obtener la ruta
 const fs = require("fs");
 const path = require("path");
+const bcrypt = require("bcryptjs");
 
 const usersFilePath = path.join(__dirname, "../data/usersDatabase.json");
 const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
@@ -13,13 +14,14 @@ const usersController = {
   },
 
   signup: (req, res) => {
+
     if (req.file) {
         const nuevoUser = {
           id: users[users.length - 1].id + 1,
           firstName: req.body.firstName,
           lastName: req.body.lastName,
           email: req.body.email,
-          password: req.body.password,
+          password: bcrypt.hashSync(req.body.password),
           category: req.body.category,
           image: req.file.image,
         };
@@ -30,7 +32,7 @@ const usersController = {
         fs.writeFileSync(usersFilePath, JSON.stringify(users, null, " "));
         
       } else {
-        let archivo = path.join(__dirname, "../views/products/product-create-form");
+        let archivo = path.join(__dirname, "../views/users/register");
         res.render(archivo, {
           users: users,
         });
