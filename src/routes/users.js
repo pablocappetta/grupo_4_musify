4a3645fa82c7db08649aa43292a73a0d1a81bc82
Nicolas Acuna <nicoacu87@gmail.com>
2021-11-28 22:02:36 -0300

    adding express-validator package

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,32 +1,31 @@
 //
 const express = require("express");
 const router = express.Router();
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
+const { check } = require("express-validator");
 
 const usersController = require("../controllers/usersController");
 
 /* === CONFIGURACIONES DE MULTER PARA ALMACENAMIENTO DE IMGS === */
 
 const multerDiskStorage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    let folder = path.join(__dirname, "../../public/img/producer-img"); // Multer guardará acá las fotos enviadas por el form
+    callback(null, folder);
+  },
 
-    destination:(req, file, callback) => {
-        let folder = path.join(__dirname, '../../public/img/producer-img'); // Multer guardará acá las fotos enviadas por el form
-        callback(null, folder);
-    },
-
-    fileName: (req, file, callback) => {
-        let imageName = Date.now() + path.extname(file.originalname);
-        callback(null, imageName);
-    } 
+  fileName: (req, file, callback) => {
+    let imageName = Date.now() + path.extname(file.originalname);
+    callback(null, imageName);
+  },
 });
 
-let fileUpload = multer({storage: multerDiskStorage});
+let fileUpload = multer({ storage: multerDiskStorage });
 
 router.get("/login", usersController.login);
 router.get("/register", usersController.register);
-router.post("/register", fileUpload.single("image"), usersController.signup)
-
+router.post("/register", fileUpload.single("image"), usersController.signup);
 
 // Tengo que exportar el router
 module.exports = router;
