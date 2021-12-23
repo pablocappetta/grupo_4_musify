f48b45959294be059923c8fbcd322574dc73f0c3
Felix-Hz <feluhv@outlook.es>
2021-12-22 22:33:09 -0300

    modified sql script

--- src/data/scripts/database.sql
+++ src/data/scripts/database.sql
@@ -1,28 +1,47 @@
-CREATE DATABASE musifydb;
+CREATE DATABASE musify_db;
 
-CREATE TABLE ´Products´ (
-	products_id INTEGER NOT NULL AUTO_INCREMENT,
-	name VARCHAR(255) NOT NULL,
+CREATE TABLE `Products` (
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	product_name VARCHAR(255) NOT NULL,
 	price FLOAT NOT NULL,
 	discount DECIMAL(5,2),
 	producer VARCHAR(255) NOT NULL,
-	genre VARCHAR(255),
-	description_product VARCHAR(255),
-	image_product VARCHAR(2048),
+	product_description VARCHAR(255),
+	product_image VARCHAR(2048),
 	popularity INTEGER,
-	PRIMARY KEY (products_id),
-	FOREIGN KEY (users_id) REFERENCES Users(users_id)
+	PRIMARY KEY (id),
+	FOREIGN KEY (users_id) REFERENCES Users(id)
+	FOREIGN KEY (genre_id) REFERENCES Genres(id)
 );
 
 CREATE TABLE `Users` (
-	users_id INTEGER NOT NULL AUTO_INCREMENT,
+	id INTEGER NOT NULL AUTO_INCREMENT,
 	first_name NVARCHAR(64) NOT NULL,
 	last_name NVARCHAR(64) NOT NULL,
 	email VARCHAR(320) NOT NULL UNIQUE,
 	password VARCHAR(64) NOT NULL,
-	category VARCHAR(32) NOT NULL,
 	description_producer VARCHAR(255),
 	image_producer VARCHAR(2048),
-	admin_privilege BOOLEAN,
-	PRIMARY KEY (users_id)
+	PRIMARY KEY (id)
+	FOREIGN KEY (category_id) REFERENCES User_category(id)
+);
+
+CREATE TABLE `Genres` (
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	genre_name NVARCHAR(32) NOT NULL UNIQUE,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE `User_category` (
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	user_type NVARCHAR(32) NOT NULL UNIQUE,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE `Cart` (
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	price FLOAT NOT NULL,
+	PRIMARY KEY (id)
+	FOREIGN KEY (users_id) REFERENCES Users(id),
+	FOREIGN KEY (products_id) REFERENCES Products(id)
 );
